699eafb71a577287ecac0c8ed81d54bd72850259
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-05-24 12:50:12 +0700

    add add friend delete friend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 
 const authRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
+const friendRouter = require('./routes/friendRoute');
 const authenticate = require('./middlewares/authenticate');
 const notFoundMiddleware = require('./middlewares/notFound');
 const errorMiddleware = require('./middlewares/error');
@@ -24,6 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/auth', authRouter);
 app.use('/users', authenticate, userRouter);
+app.use('/friends', authenticate, friendRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
