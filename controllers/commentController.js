6b750c9d709f1b7e1c9dc746bf98985edbebf0db
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-05-25 16:32:10 +0700

    complete all feature

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -21,7 +21,7 @@ exports.updateComment = async (req, res, next) => {
   try {
     const { title } = req.body;
     const { id, postId } = req.params;
-    const comment = await Comment.findOne({ id, postId });
+    const comment = await Comment.findOne({ where: { id, postId } });
     if (!comment) {
       createError('comment not found', 400);
     }
@@ -41,7 +41,7 @@ exports.updateComment = async (req, res, next) => {
 exports.deleteComment = async (req, res, next) => {
   try {
     const { id, postId } = req.params;
-    const comment = await Comment.findOne({ id, postId });
+    const comment = await Comment.findOne({ where: { id, postId } });
     if (!comment) {
       createError('comment not found', 400);
     }
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -7,6 +7,8 @@ const commentController = require('../controllers/commentController');
 const router = express.Router();
 
 router.post('/', upload.single('image'), postController.createPost);
+router.patch('/:id', upload.single('image'), postController.updatePost);
+router.delete('/:id', postController.deletePost);
 router.post('/:postId/like', postController.createLike);
 router.delete('/:postId/like', postController.deleteLike);
 router.post('/:postId/comments', commentController.createComment);
