a088ccde0ba688e2948ed3b0697b600c96225d67
pisutthee <pisut.flyinggiraffe@gmail.com>
2022-05-25 11:59:49 +0700

    add create like

--- app.js
+++ app.js
@@ -6,12 +6,10 @@ const morgan = require('morgan');
 const authRouter = require('./routes/authRoute');
 const userRouter = require('./routes/userRoute');
 const friendRouter = require('./routes/friendRoute');
+const postRouter = require('./routes/postRoute');
 const authenticate = require('./middlewares/authenticate');
 const notFoundMiddleware = require('./middlewares/notFound');
 const errorMiddleware = require('./middlewares/error');
-
-// const { sequelize } = require('./models');
-// sequelize.sync({ force: true });
 
 const app = express();
 app.use(cors());
@@ -26,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/auth', authRouter);
 app.use('/users', authenticate, userRouter);
 app.use('/friends', authenticate, friendRouter);
+app.use('/posts', authenticate, postRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
